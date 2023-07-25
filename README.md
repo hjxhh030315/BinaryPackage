bf64d7831d305b542116c1378e97d68a738f10f2
hjxhh030315 <jessica.jx.he@gmail.com>
2023-07-26 00:42:46 +0800

    finish documenting

--- README.md
+++ README.md
@@ -7,10 +7,35 @@ Binary data is the most basic and fundamental form of data. It's represented usi
 This Binarypackage is designed to provide an efficient and intuitive way to manipulate binary data in Python. It provides a Binary class that allows to perform various operations on binary data, such as swapping byte order (endianness), applying byte padding, performing bit-level operations, and coverting binary data to and from hex, int and byte string formats. The package aims to simplify working with binary data and bit manipulations.
 
 ## explanation of the Binary object and its properties. ##
+
 The Binary object is a representation of binary data in Python. It's an instance of the Binary class, which provides a convenient interface for manipulating binary data.
 
 The Binary object has two main properties:
 
 value: This property holds the actual binary data. It's stored as an integer, which makes it easy to perform various binary operations on the data.
 
-byte_order: This property specifies the byte order (endianness) of the data. It can be either 'big' or 'little', which refers to big-endian and little-endian, respectively. Big-endian means that the most significant byte is stored in the smallest address, while little-endian means that the least significant byte is stored in the smallest address.
+byte_order: This property specifies the byte order (endianness) of the data. It can be either 'big' or 'little', which refers to big-endian and little-endian.
+
+### Creating Binary Objects from Integer Values ###
+You can create a Binary object from an integer value of any size. Here's an example:
+
+```python
+# Create a Binary object from a small integer value
+data = Binary(123)
+# Create a Binary object from a large integer value
+data = Binary(12345678901234567890)
+```
+
+When creating a Binary object, you can optionally specify the byte order of the data:
+
+```python
+Copy code
+# with big-endian byte order
+data = Binary(123, 'big')
+# with little-endian byte order
+data = Binary(123, 'little')
+```
+
+## How to run test file ##
+
+cd to the package then run python3 -m unittest test.test_binary
--- binary/binary.py
+++ binary/binary.py
@@ -1,5 +1,15 @@
 class Binary:
+    """
+    Binary class for manipulation of binary data.
+    value (int): The binary data value, represented as an integer.
+    byte_order (str): The byte order ('big' or 'little') of the data.
+    """
     def __init__(self, value, byte_order='big'):
+        """
+        Initialize a Binary object.
+        value (int): The binary data, represented as an integer.
+        byte_order (str, optional): The byte order of the data. Defaults to 'big'.
+        """
         if isinstance(value, int):
             if value < 0:
                 raise ValueError("Value must be a positive integer.")
@@ -14,30 +24,60 @@ class Binary:
             raise ValueError("Value must be a positive integer, a list of bytes, a bytes object, a binary string, a hexadecimal string, or a bytes literal")
         self.byte_order = byte_order
 
-#coverts object's data to bytes
     def to_bytes(self, length, byte_order=None):
+        """
+        Converts the Binary object's data to bytes.
+        length (int): The desired number of bytes.
+        byte_order (str, optional): The byte order to use. Defaults to the object's byte_order.
+        Returns bytes: The byte representation of the object's data.
+        """
         byte_order = byte_order or self.byte_order
         return self.value.to_bytes(length, byteorder=byte_order)
 
-#reads a byte object back to object
     def from_bytes(self, byte_data, byte_order=None):
+        """
+        Reads a bytes object back into the Binary object.
+        byte_data (bytes): The bytes object to read.
+        byte_order (str, optional): The byte order of byte_data. Defaults to the object's byte_order.
+        """
         byte_order = byte_order or self.byte_order
         self.value = int.from_bytes(byte_data, byteorder=byte_order)
 
-#supports for bit fields
     def bit_field(self, start, end):
+        """
+        Returns a specific bit field from the object's data.
+        start (int): The start position of the bit field.
+        end (int): The end position of the bit field.
+        Returns int: The value of the bit field.
+        """
         mask = (2 ** (end - start + 1) - 1) << start
         return (self.value & mask) >> start
 
     def set_bit(self, position, value):
+        """
+        Sets the value of a specific bit.
+        position (int): The position of the bit to set.
+        value (int): The new value of the bit (1 or 0).
+        """
         value = 1 if value else 0
         mask = value << position
         self.value = self.value & ~(1 << position) | mask
 
     def get_bit(self, position):
+        """
+        Gets the value of a specific bit.
+        position (int): The position of the bit to set.
+        """
         return (self.value >> position) & 1
     
     def bit_operation(self, operation, other):
+        """
+        Performs a bit-level operation on the Binary object's data.
+        operation (str): The operation to perform ('and', 'or', 'xor', 'not', 'shift_left', or 'shift_right').
+        other (int): The other operand for the operation. Not used for 'not'.
+        Returns int: The result of the operation.
+        Raises ValueError: If operation is not a valid operation.
+        """
         if operation == 'and':
             return self.value & other
         elif operation == 'or':
@@ -58,18 +98,38 @@ class Binary:
             raise ValueError("Invalid operation. Available operations are only 'and', 'or', 'xor', 'not', 'shift_left', 'shift_right'.")
     
     def to_hex(self):
+        """
+        Converts the Binary object's data to a hexadecimal string.
+        Returns str: The hexadecimal representation of the data.
+        """
         return hex(self.value)
 
     def from_hex(self, hex_string):
+        """
+        Reads a hexadecimal string into the Binary object's data.
+        hex_string (str): The hexadecimal string to read.
+        """
         self.value = int(hex_string, 16)
 
     def to_bin(self):
+        """
+        Converts the Binary object's data to a binary string.
+        Returns str: The binary representation of the data.
+        """
         return bin(self.value)
 
     def from_bin(self, bin_string):
+        """
+        Reads a binary string into the Binary object's data.
+        bin_string (str): The binary string to read.
+        """
         self.value = int(bin_string, 2)
 
     def byte_swap(self):
+        """
+        Swaps the byte order (endianness) of the Binary object's data.
+        Raises ValueError: If the byte length of the data is odd.
+        """
         byte_length = (self.value.bit_length() + 7) // 8
         if byte_length % 2 != 0:
             raise ValueError("Cannot swap an odd number of bytes.")
@@ -79,6 +139,11 @@ class Binary:
         self.byte_order = 'big' if self.byte_order == 'little' else 'little'
 
     def byte_padding(self, align):
+        """
+        Aligns the Binary object's data to a specific byte boundary by adding padding.
+        align (int): The alignment for the byte boundary.
+        Raises ValueError: If align is not greater than 0.
+        """
         if align <= 0:
             raise ValueError("Alignment must be greater than 0.")
         byte_length = (self.value.bit_length() + 7) // 8
